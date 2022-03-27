3f084e93d8f16d1ced9a1cd7e2c723704807234a
Matheus Toledo <m.toledo@nextage.com.br>
2022-03-27 20:55:31 -0300

    ✏️ Corrige .stop

--- src/commands/stop.js
+++ src/commands/stop.js
@@ -3,6 +3,7 @@ import { playCards } from "../config/messages.js"
 
 export default async function stop(message) { 
   const serverQueue = QUEUES_LIST.get(message.guild.id)
+  serverQueue.songs = []
   stopPlaying()
   serverQueue.textChannel.send({embeds: [playCards.stop()]})
   return serverQueue.connection.disconnect()
